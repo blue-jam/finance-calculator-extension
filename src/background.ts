138168b86ea36ba3dcff85721be79705b3b4c219
blue_jam <blue-jam@users.noreply.github.com>
2020-11-25 19:30:39 +0900

    Removeg unnecessary permissions

--- src/background.ts
+++ src/background.ts
@@ -10,16 +10,4 @@ chrome.runtime.onInstalled.addListener(function () {
             });
         }
     );
-    chrome.declarativeContent.onPageChanged.removeRules(undefined, function () {
-        chrome.declarativeContent.onPageChanged.addRules([
-            {
-                conditions: [
-                    new chrome.declarativeContent.PageStateMatcher({
-                        pageUrl: { hostEquals: 'site3.sbisec.co.jp' },
-                    }),
-                ],
-                actions: [new chrome.declarativeContent.ShowPageAction()],
-            },
-        ]);
-    });
 });
